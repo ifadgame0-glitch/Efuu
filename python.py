1e03040d351298ade59af2a87988423aaebf306f
ifadgame0-glitch <ifadgame0@gmail.com>
2026-06-06 02:40:07 +0600

    Create python.py

---
+++ python.py
@@ -0,0 +1,11 @@
+import os
+
+banner = r"""
+╔══════════════════════════════╗
+║        TERMUX SETUP          ║
+║       Created By Efuu        ║
+╚══════════════════════════════╝
+"""
+
+os.system("clear")
+print(banner)
